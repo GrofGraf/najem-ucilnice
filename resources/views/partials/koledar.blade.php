b811fdb41b97769b4b6e22a58708038a3bb0c58e
Grof Graf <miklavz.grafenauer@gmail.com>
2017-05-16 19:39:24 +0200

    minified and compiled css and js

--- resources/views/partials/koledar.blade.php
+++ resources/views/partials/koledar.blade.php
@@ -2,6 +2,6 @@
   <h1>KOLEDAR ZASEDENOSTI</h1>
   <div class="content-margin">
     <div id="calendar"></div>
-    <small>Ob navedenih urah učilnice ni več mogoče rezervirati.</small>
+    <small>Ob navedenih terminih učilnice ni več mogoče rezervirati.</small>
   </div>
 </div>
