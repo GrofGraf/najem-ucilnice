6f0e808dbbf26829bea70e97bf2d76e5033c0110
Grof Graf <miklavz.grafenauer@gmail.com>
2017-06-15 16:50:09 +0200

    zamenjane slike za nove

--- resources/views/partials/image-modal.blade.php
+++ resources/views/partials/image-modal.blade.php
@@ -4,13 +4,13 @@
     <img class="modal-content" id="image">
     <div id="caption">
       <div onclick="selectImage(this, 0)">
-        <img src="{!! env('STATIC_URL') !!}/images/thumbnails/inter-es-najem-ucilnice-1.jpg">
+        <img src="{!! env('STATIC_URL') !!}/images/thumbnails/najem-ucilnice-tabla.jpg">
       </div>
       <div onclick="selectImage(this, 1)">
-        <img src="{!! env('STATIC_URL') !!}/images/thumbnails/inter-es-najem-ucilnice-2.jpg">
+        <img src="{!! env('STATIC_URL') !!}/images/thumbnails/najem-ucilnice-od-zadaj.jpg">
       </div>
       <div onclick="selectImage(this, 2)">
-        <img src="{!! env('STATIC_URL') !!}/images/thumbnails/inter-es-najem-ucilnice-3.jpg">
+        <img src="{!! env('STATIC_URL') !!}/images/thumbnails/najem-ucilnice-od-spredaj.jpg">
       </div>
     </div>
   </div>
