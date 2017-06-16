a5c819db2718a5a5a74c991d607aff5917ca5dc2
Grof Graf <miklavz.grafenauer@gmail.com>
2017-06-16 09:37:26 +0200

    se en test z javascriptom

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
       <meta http-equiv="X-UA-Compatible" content="IE=edge">
       <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=3">
       <title>Najem ucilnic</title>
-      <link rel="stylesheet" href="{!! env('STATIC_URL') !!}/css/main.css" media="none">
+      <link rel="stylesheet" href="{!! env('STATIC_URL') !!}/css/main.css">
     </head>
     <body style="background-color:#E8E8E8">
       @include('partials.navbar')
@@ -42,9 +42,8 @@
         @include('partials.image-modal')
       </div>
 
-      <link rel="stylesheet" href="{!! env('STATIC_URL') !!}/css/main.css">
       <script src="{!! env('STATIC_URL') !!}/js/main.js"></script>
       <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.18.1/locale/sl.js"></script>
-      <script src="https://maps.googleapis.com/maps/api/js?key={!! env('GOOGLE_MAPS_KEY')!!}&callback=myMap"></script>
+      <script src="https://maps.googleapis.com/maps/api/js?key={!! env('GOOGLE_MAPS_KEY')!!}&callback=myMap" async></script>
     </body>
 </html>
